a89585a3ce256b2f3583535de198021b1b11ed3a
derekscode <17056283+derekscode@users.noreply.github.com>
2020-09-23 12:54:07 -0700

    progress

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from "react-dom";
 // import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
 
-    // only time we do direct assignment to this.state
-    this.state = {
-      lat: null,
-      errorMessage: ""
-    };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
+      (position) => this.setState({ lat: position.coords.latitude }),
       (err) => this.setState({ errorMessage: err.message })
     );
   }
+
+  componentDidUpdate() {
+    console.log("My component was just updated - it rerendered!");
+  }
+
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
